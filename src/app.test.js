67c4c4c89fab68ee188293ea1d0b5b381813e1b7
Punnathorn Saraphol <punnathorn02zz@gmail.com>
2024-04-24 17:50:58 +0700

    try to edit tuesday

--- src/app.test.js
+++ src/app.test.js
@@ -3,7 +3,7 @@ const { dayOfTheWeek } = require('./app');
 
 test('getDay returns the long-format day of the week', () => {
     const day = dayOfTheWeek( new Date('3/11/2020') );
-    expect( day ).toBe('Wednesday');
+    expect( day ).toBe('Tuesday');
 });
 test('getMonth returns the month of the year', () => {
     const month = monthOfTheYear( new Date('3/11/2020') );
